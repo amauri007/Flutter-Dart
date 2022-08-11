41342323265c0946b6d1249d5b7a2012fc487928
Amauri Pereira Sousa <amaurimajor100@gmail.com>
2022-08-10 23:13:59 -0300

    Primeira Pagina

--- flutter_iniciantes_principais_widgets/lib/home_page.dart
+++ flutter_iniciantes_principais_widgets/lib/home_page.dart
@@ -1,1 +1,26 @@
-Class HomePage 
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: Text(
+            'Maury Show',
+            style: TextStyle(
+              color: Colors.red,
+            ),
+          ),
+          actions: [
+            Center(
+              child: Text('XXXXXXXX'),
+            ),
+            Center(
+              child: Text('YYYYYYYYY'),
+            )
+          ]),
+      drawer: Drawer(),
+    );
+  }
+}
