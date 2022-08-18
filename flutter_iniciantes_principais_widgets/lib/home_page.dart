5006a1220f7f9c159c769501d62195511153278d
Amauri Pereira Sousa <amaurimajor100@gmail.com>
2022-08-17 21:30:26 -0300

    implementando cross

--- flutter_iniciantes_principais_widgets/lib/home_page.dart
+++ flutter_iniciantes_principais_widgets/lib/home_page.dart
@@ -5,28 +5,37 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-          title: Text(
-            'Maury Show',
-            style: TextStyle(
-              color: Colors.red,
-              fontSize: 30,
+        appBar: AppBar(
+            title: Text(
+              'Maury Show',
+              style: TextStyle(
+                color: Colors.red,
+                fontSize: 30,
+              ),
             ),
+            actions: [
+              Center(
+                child: Text('XXXXX'),
+              ),
+              Center(
+                child: Text('YYYYYY'),
+              )
+            ]),
+        drawer: Drawer(),
+        body: Container(
+          width: 200,
+          height: 200,
+          color: Colors.green,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Text('Teste'),
+              Text('Teste'),
+              Text('Teste'),
+              Text('Teste'),
+            ],
           ),
-          actions: [
-            Center(
-              child: Text('XXXXX'),
-            ),
-            Center(
-              child: Text('YYYYYY'),
-            )
-          ]),
-      drawer: Drawer(),
-      whidth: 200,
-      height: 200,
-      color: Colors.green,
-      child: Text('Teste'),
-      body: Container(child: Text('Teste')),
-    );
+        ));
   }
 }
